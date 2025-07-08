041ef390b96dd91c1b5a1710698d9bf6f47d596b
Sameer Alam <sameeralam57k@gmail.com>
2025-07-08 23:54:34 +0530

    build pages

--- src/components/index.js
+++ src/components/index.js
@@ -6,5 +6,22 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from "./Select";
 import RTE from "./RTE";
+import Login from "./Login";
+import Singup from "./Singup";
+import Postcard from "./PostCard";
+import PostForm from "./postform/PostForm";
 
-export { Header, Footer, Logo, Container, Button, Input, Select, RTE };
+export {
+  Header,
+  Footer,
+  Logo,
+  Container,
+  Button,
+  Input,
+  Select,
+  RTE,
+  Singup,
+  Login,
+  PostForm,
+  Postcard,
+};
---
+++ src/pages/Singup.jsx
@@ -0,0 +1,11 @@
+import { Singup as SingupComponenet } from "../components";
+
+function Singup() {
+  return (
+    <div className="py-8">
+      <SingupComponenet />
+    </div>
+  );
+}
+
+export default Singup;
